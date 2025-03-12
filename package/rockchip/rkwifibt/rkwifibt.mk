caa3fbb2eb8a7cda78e5b8493ce9d9467a92ece6
Mitchell Ma <machuang@radxa.com>
2025-03-12 17:51:24 +0800

    package: rockchip: rkwifibt: add aic firmware
    Signed-off-by: Mitchell Ma <machuang@radxa.com>

--- package/rockchip/rkwifibt/rkwifibt.mk
+++ package/rockchip/rkwifibt/rkwifibt.mk
@@ -15,4 +15,27 @@ RKWIFIBT_CFLAGS = $(TARGET_CFLAGS) -static
 RKWIFIBT_LDFLAGS = $(TARGET_LDFLAGS) -static
 endif
 
+ifeq ($(BR2_PACKAGE_RKWIFIBT_AIC8800D80_SDIO_FIRMWARE),y)
+RKWIFIBT_AIC_FIRMWARE_DIRS += firmware/aic/sdio/aic8800D80
+endif
+
+ifeq ($(BR2_PACKAGE_RKWIFIBT_AIC8800D80_USB_FIRMWARE),y)
+RKWIFIBT_AIC_FIRMWARE_DIRS += firmware/aic/usb/aic8800D80
+endif
+
+ifneq ($(RKWIFIBT_AIC_FIRMWARE_DIRS),)
+define AIC_FIRMWARE_INSTALL_DIRS
+	$(foreach d,$(RKWIFIBT_AIC_FIRMWARE_DIRS), \
+		cp -a $(@D)/$(d)/* $(TARGET_DIR)/lib/firmware/aic/$(sep))
+endef
+endif
+
+define AIC_FIRMWARE_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/lib/firmware/aic
+	mkdir -p $(TARGET_DIR)/lib/firmware/aic
+	$(AIC_FIRMWARE_INSTALL_DIRS)
+endef
+
+RKWIFIBT_POST_INSTALL_TARGET_HOOKS += AIC_FIRMWARE_INSTALL_TARGET_CMDS
+
 $(eval $(meson-package))
